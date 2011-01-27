026b44b718b6124e897eb61c6d12a0254db90ead
Carlo Bersani <carlocci@gmail.com>
2011-01-27 16:13:10 +0100

    Fixed indentation

--- qunction.js
+++ qunction.js
@@ -13,22 +13,22 @@ function Qunction(queue, latency, context) {
     q[s++].call(context)
     setTimeout(wrapper, latency)}
 
-  this.getQueue = function() {return q}
-  this.add      = function(funcs, position) {
-                    if (typeof funcs === 'function') funcs = [funcs]
-                    if (funcs instanceof Qunction) funcs = funcs.getQueue()
-                    if (position === undefined)    position = q.length
-                    Array.prototype.splice.apply(q, [position, 0].concat(funcs))
-                    if (run) setTimeout(wrapper, latency)
-                    return this}
-  this.remove   = function(f, all) {
-                    var i, l
-                    if (+f === f) q.splice(f,1)
-                    else for (i=0, l=q.length; i < l; --i) 
-                      if (f === q[i]) {
-                        q.splice(i, 1)
-                        if (!all) break}
-                    return this}
+  this.getQueue   = function() {return q}
+  this.add        = function(funcs, position) {
+                      if (typeof funcs === 'function') funcs = [funcs]
+                      if (funcs instanceof Qunction) funcs = funcs.getQueue()
+                      if (position === undefined)    position = q.length
+                      Array.prototype.splice.apply(q, [position, 0].concat(funcs))
+                      if (run) setTimeout(wrapper, latency)
+                      return this}
+  this.remove     = function(f, all) {
+                      var i, l
+                      if (+f === f) q.splice(f,1)
+                      else for (i=0, l=q.length; i < l; --i) 
+                        if (f === q[i]) {
+                          q.splice(i, 1)
+                          if (!all) break}
+                      return this}
   this.start      = function() {run = true; setTimeout(wrapper, latency); return this}
   this.pause      = function() {run = false; return this}
   this.stop       = function() {run = false, s = 0; return this}
