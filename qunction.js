aaa302009c8bc948d95f49e3a23ff4041a04e8fa
Carlo Bersani <carlocci@gmail.com>
2011-01-27 01:24:51 +0100

    Single add function, minor fixes

--- qunction.js
+++ qunction.js
@@ -8,32 +8,30 @@ function Qunction(queue) {
     , latency = 0
 
   function wrapper() {
-    if (!run) return
+    if (!run || s === q.length) return
     q[s++]()
     setTimeout(wrapper, latency)}
 
   this.getQueue = function() {return q}
-  
-  this.add      = function(func, position) {
-                    if (position === undefined) q.push(func)
-                    else                        q.splice(position, 0, func)}
-
-  this.adds     = function(funcs, position) {
+  this.add      = function(funcs, position) {
+                    if (typeof funcs === 'function') funcs = [funcs]
                     if (funcs instanceof Qunction) funcs = funcs.getQueue()
                     if (position === undefined)    position = q.length
-                    Array.prototype.splice.apply(q, [position, 0].concat(funcs))}
-
+                    Array.prototype.splice.apply(q, [position, 0].concat(funcs))
+                    if (run) setTimeout(wrapper, latency)
+                    return this}
   this.remove   = function(f, all) {
                     var i, l
                     if (+f === f) q.splice(f,1)
                     else for (i=0, l=q.length; i < l; --i) {
                       if (f === q[i]) {
                         q.splice(i, 1)
-                        if (!all) break;}}}
-
-  this.start    = function() {setTimeout(wrapper, latency)}
-  this.pause    = function() {run = false}
-  this.stop     = function() {run = false, s = 0}
+                        if (!all) break}}
+                    return this}
+  this.start    = function() {run = true; setTimeout(wrapper, latency); return this}
+  this.pause    = function() {run = false; return this}
+  this.stop     = function() {run = false, s = 0; return this}
+  this.rewind   = function() {s = 0; return this}
 }
 
 window.Qunction = Qunction;
